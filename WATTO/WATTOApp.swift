aa291ac7b1db3b48f76340c2c59d6aea8bc379a1
Matt Gaidica, Ph.D <matt@gaidi.ca>
2023-04-10 19:08:28 -0400

    Initial Commit

---
+++ WATTO/WATTOApp.swift
@@ -0,0 +1,17 @@
+//
+//  WATTOApp.swift
+//  WATTO
+//
+//  Created by Matt Gaidica on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WATTOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
