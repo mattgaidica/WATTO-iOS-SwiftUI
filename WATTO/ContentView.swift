098dad45c16e73b14b4bc253704fd7e0c7c2a37c
Matt Gaidica, Ph.D <matt@gaidi.ca>
2023-04-11 14:54:21 -0400

    Fully working

--- WATTO/ContentView.swift
+++ WATTO/ContentView.swift
@@ -3,31 +3,146 @@ import Charts
 
 struct ContentView: View {
     @ObservedObject private var bleManager = BLEManager()
-
-    var body: some View {
-        VStack {
-            plotView(title: "Voltage", data: bleManager.voltageData)
-            plotView(title: "Current", data: bleManager.currentData)
-            plotView(title: "Power", data: bleManager.powerData)
-        }
-        .navigationBarTitle("My Charts", displayMode: .inline)
-    }
+    let plotHeight:CGFloat = 175.0
     
-    private func plotView(title: String, data: [Float]) -> some View {
-        List {
+    var body: some View {
+        let bins = bleManager.powerScale(start: 30, end: 4000.0, points: 51, exponent: 3)
+        
+        VStack {
             Section {
-                Text(title)
-                DataPlot(title: title, data: data)
-                    .frame(height: 150)
+                Toggle("Scan for Watto peripheral?", isOn: $bleManager.doScan)
+            }.padding(EdgeInsets(top: 10, leading: 60, bottom: 20, trailing: 60))
+            
+            Section(header: Text("Current (µA)").fontWeight(.bold)) {
+                LinePlot(bleManager: bleManager, data: bleManager.currentData)
+                    .frame(height: plotHeight)
+            }
+            
+            Section() {
+                VStack {
+                    HistPlot(data: bleManager.computeHistogram(data: bleManager.currentData, bins: bins), bins: bins)
+                        .frame(height: plotHeight - 100)
+                        .padding(EdgeInsets(top: 20, leading: 0, bottom: 20, trailing: 0))
+                    CustomXAxisView(bins: bins).fixedSize(horizontal: false, vertical: true)
+                }
+            }
+            
+            Spacer()
+            
+            HStack {
+                VStack {
+                    Text("Avg (µA)").font(.footnote)
+                    Text("\(String(format: "%1.0f", bleManager.meanCurrent))").font(.title)
+                }
+                Spacer()
+                VStack {
+                    Text("Min (µA)").font(.footnote)
+                    Text("\(String(format: "%1.0f", bleManager.minCurrent))").font(.title)
+                }
+                Spacer()
+                VStack {
+                    Text("Max (µA)").font(.footnote)
+                    Text("\(String(format: "%1.0f", bleManager.maxCurrent))").font(.title)
+                }
+                Spacer()
+                VStack {
+                    Text("Power (mW)").font(.footnote)
+                    Text("\(String(format: "%1.1f", bleManager.meanPower))").font(.title)
+                }
+            }.padding()
+            
+            Spacer()
+            
+            Section {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.gray.opacity(0.1))
+                    
+                    VStack {
+                        Text("__Battery Life Estimater__: Battery Size (mAh)")
+                        Picker("Battery Size", selection: $bleManager.selectedBatterySize) {
+                            ForEach(bleManager.batterySizes, id: \.self) { size in
+                                Text("\(size)").tag(size)
+                            }
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                        .padding()
+                        
+                        Text("\(bleManager.batteryLife)").font(.title2)
+                    }
+                    .padding()
+                }
+                .padding(.horizontal)
+            }
+            
+            HStack {
+                NonEditableTextEditor(text: $bleManager.debugText)
+                    .padding(5)
+                    .frame(height: 100)
+            }.padding()
+            
+        }.padding()
+    }
+}
+
+struct HistPlot: View {
+    let data: [Float]
+    let bins: [Float]
+    
+    var body: some View {
+        GeometryReader { geometry in
+            let barWidth = max(geometry.size.width / CGFloat(data.count), 1)
+            let maxValue = CGFloat(data.max() ?? 1)
+            let allZeros = data.allSatisfy { $0 == 0 }
+            
+            HStack(alignment: .center, spacing: 0) {
+                ForEach(data.indices, id: \.self) { index in
+                    VStack(spacing: 0) {
+                        RoundedRectangle(cornerRadius: 3)
+                            .fill(Color.blue)
+                            .frame(width: barWidth, height: allZeros ? 0 : max(CGFloat(data[index]) * geometry.size.height / maxValue, 0))
+                    }
+                }
             }
         }
     }
 }
 
-struct DataPlot: View {
-    let title: String
-    let data: [Float]
+struct CustomXAxisView: View {
+    let bins: [Float]
+    let showBins = 10
     
+    var body: some View {
+        GeometryReader { geometry in
+            HStack(spacing: 0) {
+                let step = max(bins.count / showBins, 1)
+                ForEach(Array(stride(from: 0, to: bins.count, by: step)), id: \.self) { index in
+                    Text(customFormatter(value: bins[index]))
+                        .frame(width: geometry.size.width / CGFloat(showBins + 1))
+                        .rotationEffect(.degrees(90))
+                        .foregroundColor(.gray)
+                        .font(.footnote)
+                    
+                    if index != bins.count - 1 {
+                        Spacer(minLength: 0)
+                    }
+                }
+            }
+        }
+    }
+    
+    private func customFormatter(value: Float) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.maximumFractionDigits = value >= 100 ? 0 : 1
+        
+        return numberFormatter.string(from: NSNumber(value: value)) ?? ""
+    }
+}
+
+struct LinePlot: View {
+    @ObservedObject var bleManager: BLEManager
+    let data: [Float]
     var body: some View {
         Chart {
             ForEach(data.indices, id: \.self) { index in
@@ -37,25 +152,21 @@ struct DataPlot: View {
                         y: .value("y", Double(data[index]))
                     )
                 }
-                .accessibilityLabel("\(title) Line Series")
-                .accessibilityValue("X: \(index), Y: \(data[index])")
-                .accessibilityHidden(false)
             }
             .foregroundStyle(.red)
         }
-        .accessibilityChartDescriptor(self)
         .chartYAxis(.automatic)
-        .chartXAxis(.automatic)
+        .chartXAxis(.hidden)
+        .onTapGesture {
+            bleManager.doCollection.toggle()
+        }
     }
 }
 
-// MARK: - Accessibility
-extension DataPlot: AXChartDescriptorRepresentable {
+extension LinePlot: AXChartDescriptorRepresentable {
     func makeChartDescriptor() -> AXChartDescriptor {
-        let lineMin = data.min() ?? 0
-        let lineMax = data.max() ?? 0
-        let ymin = lineMin
-        let ymax = lineMax
+        let ymin = 0
+        let ymax = max(100, data.max() ?? 0)
         
         let xAxis = AXNumericDataAxisDescriptor(
             title: "Index",
@@ -70,7 +181,7 @@ extension DataPlot: AXChartDescriptorRepresentable {
         ) { value in "\(value)" }
         
         let lineSeries = AXDataSeriesDescriptor(
-            name: "\(title) data series",
+            name: "Data series",
             isContinuous: true,
             dataPoints: data.enumerated().map {
                 .init(x: Double($0.offset), y: Double($0.element))
@@ -78,7 +189,7 @@ extension DataPlot: AXChartDescriptorRepresentable {
         )
         
         return AXChartDescriptor(
-            title: "\(title) data",
+            title: "Data",
             summary: nil,
             xAxis: xAxis,
             yAxis: yAxis,
@@ -86,6 +197,39 @@ extension DataPlot: AXChartDescriptorRepresentable {
             series: [lineSeries]
         )
     }
+}
+
+struct NonEditableTextEditor: UIViewRepresentable {
+    @Binding var text: String
+    
+    func makeUIView(context: Context) -> UITextView {
+        let textView = UITextView()
+        textView.font = UIFont.monospacedDigitSystemFont(ofSize: UIFont.smallSystemFontSize, weight: .light)
+        textView.isEditable = false
+        textView.isSelectable = false
+        textView.text = text
+        textView.textColor = .green
+        return textView
+    }
+    
+    func updateUIView(_ uiView: UITextView, context: Context) {
+        uiView.text = text
+    }
+}
+
+func simpleMovingAverage(data: [Float], windowSize: Int) -> [Float] {
+    var smoothedData: [Float] = []
+    
+    for i in 0..<data.count {
+        let start = max(0, i - windowSize + 1)
+        let end = i + 1
+        let window = data[start..<end]
+        let sum = window.reduce(0, +)
+        let average = sum / Float(window.count)
+        smoothedData.append(average)
+    }
+    
+    return smoothedData
 }
 
 struct ContentView_Previews: PreviewProvider {
--- WATTO/WATTOApp.swift
+++ WATTO/WATTOApp.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import CoreBluetooth
 import Combine
+import Foundation
 
 @main
 struct WATTOApp: App {
@@ -19,9 +20,19 @@ struct WATTOApp: App {
 }
 
 class BLEManager: NSObject, ObservableObject, CBCentralManagerDelegate, CBPeripheralDelegate {
-    @Published var voltageData: [Float] = Array(repeating: 0.0, count: 200)
-    @Published var currentData: [Float] = Array(repeating: 0.0, count: 200)
-    @Published var powerData: [Float] = Array(repeating: 0.0, count: 200)
+    @Published var voltageData: [Float] = Array(repeating: 0.0, count: 1000)
+    @Published var currentData: [Float] = Array(repeating: 0.0, count: 1000)
+    @Published var powerData: [Float] = Array(repeating: 0.0, count: 1000)
+    @Published var doScan: Bool = false
+    @Published var debugText: String = ">> Hello, Watto"
+    @Published var meanCurrent: Float = 0
+    @Published var minCurrent: Float = 0
+    @Published var maxCurrent: Float = 0
+    @Published var meanPower: Float = 0
+    @Published var batteryLife: String = ""
+    @Published var doCollection: Bool = true
+    @Published var selectedBatterySize: Int = 40
+    let batterySizes = [20, 40, 100, 220, 1000]
     
     private var centralManager: CBCentralManager!
     private var peripheral: CBPeripheral?
@@ -30,22 +41,39 @@ class BLEManager: NSObject, ObservableObject, CBCentralManagerDelegate, CBPeriph
     private let currentCharacteristicUUID = CBUUID(string: "A002")
     private let powerCharacteristicUUID = CBUUID(string: "A003")
     
+    private var timer1Hz: Timer?
+    
     override init() {
         super.init()
         centralManager = CBCentralManager(delegate: self, queue: nil)
-    }
-    
-    func startScanning() {
-        if centralManager.state == .poweredOn {
-            centralManager.scanForPeripherals(withServices: [serviceUUID], options: nil)
-        }
+        self.batteryLife = formattedBatteryLife(batterySize: self.selectedBatterySize, microAmps: self.meanCurrent)
     }
     
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         if central.state == .poweredOn {
-            startScanning()
+            timer1Hz = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                if self.doScan && self.peripheral == nil {
+                    self.centralManager.scanForPeripherals(withServices: [self.serviceUUID], options: nil)
+                    self.dprint("Scanning for Watto peripheral")
+                }
+                if !self.doScan {
+                    self.centralManager.stopScan()
+                    if let peripheral = self.peripheral {
+                        self.centralManager.cancelPeripheralConnection(peripheral)
+                        self.dprint("Cancelling scan/connnection")
+                    }
+                }
+                if let _ = self.peripheral {
+                    self.meanCurrent = self.currentData.reduce(0, +) / Float(self.currentData.count)
+                    self.minCurrent = self.currentData.min() ?? 0
+                    self.maxCurrent = self.currentData.max() ?? 0
+                    self.meanPower = self.powerData.reduce(0, +) / Float(self.powerData.count) / 1000
+                    self.batteryLife = self.formattedBatteryLife(batterySize: self.selectedBatterySize, microAmps: self.meanCurrent)
+                }
+            }
+            
         } else {
-            print("Central Manager is not powered on")
+            dprint("Central Manager is not powered on")
         }
     }
     
@@ -61,7 +89,14 @@ class BLEManager: NSObject, ObservableObject, CBCentralManagerDelegate, CBPeriph
     }
     
     func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
-        print("Failed to connect to peripheral:", error.debugDescription)
+        doScan = false
+        dprint("Failed to connect to peripheral: \(error.debugDescription)")
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        self.peripheral = nil
+        doScan = false
+        dprint("Disconnected from \(peripheral.name ?? "")")
     }
     
     func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
@@ -79,42 +114,116 @@ class BLEManager: NSObject, ObservableObject, CBCentralManagerDelegate, CBPeriph
             for characteristic in characteristics {
                 if characteristic.properties.contains(.notify) {
                     peripheral.setNotifyValue(true, for: characteristic)
+                    dprint("Notifying on \(characteristic.uuid)")
                 }
             }
         }
     }
     
     func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
-        if let data = characteristic.value {
-            var floatArray: [Float] = []
-            let count = data.count / MemoryLayout<Float>.size
-            floatArray = [Float](repeating: 0.0, count: count)
-            data.withUnsafeBytes { bufferPointer in
-                let floatBufferPointer = bufferPointer.bindMemory(to: Float.self)
-                floatArray = Array(floatBufferPointer)
-            }
-            
-            DispatchQueue.main.async {
-                switch characteristic.uuid {
-                case self.voltageCharacteristicUUID:
-                    self.updateData(data: &self.voltageData, newData: floatArray)
-                case self.currentCharacteristicUUID:
-                    self.updateData(data: &self.currentData, newData: floatArray)
-                case self.powerCharacteristicUUID:
-                    self.updateData(data: &self.powerData, newData: floatArray)
-                default:
-                    break
+        if doCollection {
+            if let data = characteristic.value {
+                var floatArray: [Float] = []
+                let count = data.count / MemoryLayout<Float>.size
+                floatArray = [Float](repeating: 0.0, count: count)
+                data.withUnsafeBytes { bufferPointer in
+                    let floatBufferPointer = bufferPointer.bindMemory(to: Float.self)
+                    floatArray = Array(floatBufferPointer)
+                }
+                
+                DispatchQueue.main.async {
+                    switch characteristic.uuid {
+                    case self.voltageCharacteristicUUID:
+                        self.updateData(data: &self.voltageData, newData: floatArray)
+                    case self.currentCharacteristicUUID:
+                        self.updateData(data: &self.currentData, newData: floatArray)
+                    case self.powerCharacteristicUUID:
+                        self.updateData(data: &self.powerData, newData: floatArray)
+                    default:
+                        break
+                    }
                 }
             }
         }
     }
     
     private func updateData(data: inout [Float], newData: [Float]) {
-        let maxLength = 200
-        let overflow = data.count + newData.count - maxLength
+        let overflow = data.count + newData.count - currentData.count
         if overflow > 0 {
             data.removeFirst(overflow)
         }
         data.append(contentsOf: newData)
     }
+    
+    private func dprint(_ addText: String) {
+        print(addText)
+        debugText = ">> \(addText)\n\(debugText)"
+    }
+    
+    func computeHistogram(data: [Float], bins: [Float]) -> [Float] {
+        let nBins = bins.count
+        var histogram = [Float](repeating: 0.0, count: nBins - 1)
+        
+        for value in data {
+            if value != 0.0 && value >= bins.first! && value <= bins.last! {
+                for binIndex in 0..<(nBins - 1) {
+                    if value >= bins[binIndex] && value < bins[binIndex + 1] {
+                        histogram[binIndex] += 1
+                        break
+                    }
+                }
+            }
+        }
+        
+        return histogram
+    }
+    
+    func formattedBatteryLife(batterySize: Int, microAmps: Float) -> String {
+        var d = 0
+        var h = 0
+        var m = 0
+        if microAmps > 0 {
+            let hours = (Float(batterySize) * 1000) / microAmps // battery is mAh
+            let totalSeconds = Int(hours * 3600)
+            d = totalSeconds / 86400 // Number of days
+            h = (totalSeconds % 86400) / 3600 // Remaining hours
+            m = (totalSeconds % 3600) / 60 // Remaining minutes
+        }
+        return "\(d) days, \(h) hours, \(m) minutes"
+    }
+    
+    func logspace(start: Float, end: Float, points: Int) -> [Float] {
+        var startLog = start
+        if start > 0 {
+            startLog = log10(start)
+        }
+        let endLog = log10(end)
+        let step = (endLog - startLog) / Float(points - 1)
+        
+        var logScaledValues: [Float] = []
+        
+        for index in 0...(points - 1) {
+            let exponent = startLog + step * Float(index)
+            let value = pow(10, exponent)
+            logScaledValues.append(value)
+        }
+        
+        return logScaledValues
+    }
+    
+    func powerScale(start: Float, end: Float, points: Int, exponent: Float) -> [Float] {
+        let step = 1 / Float(points - 1)
+        
+        var powerScaledValues: [Float] = []
+        
+        for index in 0...(points - 1) {
+            let position = Float(index) * step
+            let powerScaledPosition = pow(position, exponent)
+            let value = start + (end - start) * powerScaledPosition
+            powerScaledValues.append(value)
+        }
+        
+        return powerScaledValues
+    }
+
 }
