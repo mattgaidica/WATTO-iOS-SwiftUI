081a096c3584505c09901589dc939a95ddd2699f
Matt Gaidica, Ph.D <matt@gaidi.ca>
2023-04-10 22:17:19 -0400

    MVP, works, needs some polishing

--- WATTO/ContentView.swift
+++ WATTO/ContentView.swift
@@ -1,21 +1,90 @@
-//
-//  ContentView.swift
-//  WATTO
-//
-//  Created by Matt Gaidica on 4/10/23.
-//
-
 import SwiftUI
+import Charts
 
 struct ContentView: View {
+    @ObservedObject private var bleManager = BLEManager()
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            plotView(title: "Voltage", data: bleManager.voltageData)
+            plotView(title: "Current", data: bleManager.currentData)
+            plotView(title: "Power", data: bleManager.powerData)
         }
-        .padding()
+        .navigationBarTitle("My Charts", displayMode: .inline)
+    }
+    
+    private func plotView(title: String, data: [Float]) -> some View {
+        List {
+            Section {
+                Text(title)
+                DataPlot(title: title, data: data)
+                    .frame(height: 150)
+            }
+        }
+    }
+}
+
+struct DataPlot: View {
+    let title: String
+    let data: [Float]
+    
+    var body: some View {
+        Chart {
+            ForEach(data.indices, id: \.self) { index in
+                Plot {
+                    LineMark(
+                        x: .value("x", index),
+                        y: .value("y", Double(data[index]))
+                    )
+                }
+                .accessibilityLabel("\(title) Line Series")
+                .accessibilityValue("X: \(index), Y: \(data[index])")
+                .accessibilityHidden(false)
+            }
+            .foregroundStyle(.red)
+        }
+        .accessibilityChartDescriptor(self)
+        .chartYAxis(.automatic)
+        .chartXAxis(.automatic)
+    }
+}
+
+// MARK: - Accessibility
+extension DataPlot: AXChartDescriptorRepresentable {
+    func makeChartDescriptor() -> AXChartDescriptor {
+        let lineMin = data.min() ?? 0
+        let lineMax = data.max() ?? 0
+        let ymin = lineMin
+        let ymax = lineMax
+        
+        let xAxis = AXNumericDataAxisDescriptor(
+            title: "Index",
+            range: Double(0)...Double(data.count - 1),
+            gridlinePositions: []
+        ) { value in "\(Int(value))" }
+        
+        let yAxis = AXNumericDataAxisDescriptor(
+            title: "Value",
+            range: Double(ymin)...Double(ymax),
+            gridlinePositions: []
+        ) { value in "\(value)" }
+        
+        let lineSeries = AXDataSeriesDescriptor(
+            name: "\(title) data series",
+            isContinuous: true,
+            dataPoints: data.enumerated().map {
+                .init(x: Double($0.offset), y: Double($0.element))
+            }
+        )
+        
+        return AXChartDescriptor(
+            title: "\(title) data",
+            summary: nil,
+            xAxis: xAxis,
+            yAxis: yAxis,
+            additionalAxes: [],
+            series: [lineSeries]
+        )
     }
 }
 
--- WATTO/WATTOApp.swift
+++ WATTO/WATTOApp.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import CoreBluetooth
+import Combine
 
 @main
 struct WATTOApp: App {
@@ -13,5 +15,106 @@ struct WATTOApp: App {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class BLEManager: NSObject, ObservableObject, CBCentralManagerDelegate, CBPeripheralDelegate {
+    @Published var voltageData: [Float] = Array(repeating: 0.0, count: 200)
+    @Published var currentData: [Float] = Array(repeating: 0.0, count: 200)
+    @Published var powerData: [Float] = Array(repeating: 0.0, count: 200)
+    
+    private var centralManager: CBCentralManager!
+    private var peripheral: CBPeripheral?
+    private let serviceUUID = CBUUID(string: "A000")
+    private let voltageCharacteristicUUID = CBUUID(string: "A001")
+    private let currentCharacteristicUUID = CBUUID(string: "A002")
+    private let powerCharacteristicUUID = CBUUID(string: "A003")
+    
+    override init() {
+        super.init()
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+    
+    func startScanning() {
+        if centralManager.state == .poweredOn {
+            centralManager.scanForPeripherals(withServices: [serviceUUID], options: nil)
+        }
+    }
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            startScanning()
+        } else {
+            print("Central Manager is not powered on")
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        self.peripheral = peripheral
+        self.peripheral?.delegate = self
+        centralManager.stopScan()
+        centralManager.connect(peripheral, options: nil)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        peripheral.discoverServices([serviceUUID])
+    }
+    
+    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+        print("Failed to connect to peripheral:", error.debugDescription)
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        if let services = peripheral.services {
+            for service in services {
+                if service.uuid == serviceUUID {
+                    peripheral.discoverCharacteristics([voltageCharacteristicUUID, currentCharacteristicUUID, powerCharacteristicUUID], for: service)
+                }
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        if let characteristics = service.characteristics {
+            for characteristic in characteristics {
+                if characteristic.properties.contains(.notify) {
+                    peripheral.setNotifyValue(true, for: characteristic)
+                }
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        if let data = characteristic.value {
+            var floatArray: [Float] = []
+            let count = data.count / MemoryLayout<Float>.size
+            floatArray = [Float](repeating: 0.0, count: count)
+            data.withUnsafeBytes { bufferPointer in
+                let floatBufferPointer = bufferPointer.bindMemory(to: Float.self)
+                floatArray = Array(floatBufferPointer)
+            }
+            
+            DispatchQueue.main.async {
+                switch characteristic.uuid {
+                case self.voltageCharacteristicUUID:
+                    self.updateData(data: &self.voltageData, newData: floatArray)
+                case self.currentCharacteristicUUID:
+                    self.updateData(data: &self.currentData, newData: floatArray)
+                case self.powerCharacteristicUUID:
+                    self.updateData(data: &self.powerData, newData: floatArray)
+                default:
+                    break
+                }
+            }
+        }
+    }
+    
+    private func updateData(data: inout [Float], newData: [Float]) {
+        let maxLength = 200
+        let overflow = data.count + newData.count - maxLength
+        if overflow > 0 {
+            data.removeFirst(overflow)
+        }
+        data.append(contentsOf: newData)
     }
 }
